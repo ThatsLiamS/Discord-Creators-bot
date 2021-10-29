9493561b22fce59ea41959084c5b7069009e83b4
William Castro <62484109+SunkenSplash@users.noreply.github.com>
2021-10-29 19:45:38 -0400

    Added response to bot ping, tabs in reload

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,8 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 	name: 'messageCreate',
 	once: false,
 
-	execute: () => {
-		/* */
+	execute: async (message, client) => {
+		
+		if (message.author.bot) return;
+
+		if ([`<@${client.user.id}>`, `<@!${client.user.id}>`].includes(message.content.trim())) {
+			
+			const embed = new MessageEmbed()
+				.setTitle('Hello there!')
+				.setDescription('I\'m the bot for Discord Creators! I operate on slash commands, so enter a slash (`/`) to use my features! You can start with `/help`!')
+				.setColor('#7289DA')
+
+			message.channel.send({ embeds: [embed] });
+
+		}
 	},
 };
